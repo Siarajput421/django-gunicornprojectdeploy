1e8610fc107232ff3519828d417070c215675a00
Siarajput421 <100112295+Siarajput421@users.noreply.github.com>
2022-05-09 17:55:17 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -24,7 +24,7 @@ ExecStart=/home/ubuntu/env/bin/gunicorn \
           --access-logfile - \
           --workers 3 \
           --bind unix:/run/gunicorn.sock \
-          first.wsgi:application
+          textutils.wsgi:application
 [Install]
 WantedBy=multi-user.target"  >  /etc/systemd/system/gunicorn.service
 
@@ -35,7 +35,7 @@ sudo systemctl enable gunicorn.socket
 
 echo "server {
     listen 80;
-    server_name 54.95.234.19;
+    server_name 3.89.104.67;
     location = /favicon.ico { access_log off; log_not_found off; }
     location /static/ {
         root /home/ubuntu/;
